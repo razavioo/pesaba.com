c98e31f1b9286420cb60bf6a5901fd24facc4ef3
razavioo <razavioo@gmail.com>
2026-07-01 14:57:33 +0330

    Fix article content images and language drawer background

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+    :src="resolvedSrc"
+    :alt="alt || ''"
+    :title="title"
+    :width="width"
+    :height="height"
+    loading="lazy"
+    decoding="async"
+  >
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  src?: string
+  alt?: string
+  title?: string
+  width?: string | number
+  height?: string | number
+}>()
+
+const { withBase } = useBaseUrl()
+const resolvedSrc = computed(() => props.src ? withBase(props.src) : '')
+</script>
